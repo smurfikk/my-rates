8cbcbedf1b38873d2a5ad72d4cbe312c1584ca1e
smurfikk <95306488+smurfikk@users.noreply.github.com>
2023-08-29 21:39:53 +0300

    update graph

--- functions/graph_func.py
+++ functions/graph_func.py
@@ -13,7 +13,7 @@ def get_graph(_type: str, from_currency: str, to_currency: str) -> str:
 def short_graph(from_currency: str, to_currency: str) -> str:
     date = get_date() - timedelta(days=2)
     conn, cursor = connect()
-    cursor.execute("SELECT strftime('%Y-%m-%d %H:00:00', date) AS hour, AVG(price) FROM rates "
+    cursor.execute("SELECT strftime('%Y-%m-%d %H:%M:%S', date) AS hour, AVG(price) FROM rates "
                    "WHERE from_currency = ? AND to_currency = ? AND date >= ? GROUP BY hour",
                    (from_currency, to_currency, date))
     data = cursor.fetchall()
@@ -24,7 +24,7 @@ def short_graph(from_currency: str, to_currency: str) -> str:
 def long_graph(from_currency: str, to_currency: str) -> str:
     date = get_date() - timedelta(days=31)
     conn, cursor = connect()
-    cursor.execute("SELECT strftime('%Y-%m-%d 00:00:00', date) AS hour, AVG(price) FROM rates "
+    cursor.execute("SELECT strftime('%Y-%m-%d %H:00:00', date) AS hour, AVG(price) FROM rates "
                    "WHERE from_currency = ? AND to_currency = ? AND date >= ? GROUP BY hour",
                    (from_currency, to_currency, date))
     data = cursor.fetchall()
@@ -35,16 +35,16 @@ def long_graph(from_currency: str, to_currency: str) -> str:
 def draw_graph(data: list[tuple], from_currency: str, to_currency: str) -> str:
     dates = [datetime.strptime(row[0], "%Y-%m-%d %H:%M:%S").strftime("%d %b %H:%M") for row in data]
     prices = [row[1] for row in data]
-    plt.figure(figsize=(10, 6))
-    plt.plot(dates, prices, marker=".")
+    plt.figure(figsize=(12, 6))
+    plt.plot(dates, prices)
     plt.xlabel("Дата")
     plt.ylabel("Цена")
     plt.title(f"Изменение курса обмена {from_currency.upper()} на {to_currency.upper()}")
-    plt.xticks(rotation=45)
+    plt.xticks(rotation=60, horizontalalignment='center')
     plt.tight_layout()
-    if len(dates) > 30:
-        plt.xticks(dates[::2])
-    plt.grid(True)
+    if len(dates) > 35:
+        plt.xticks(dates[::len(dates) // 35])
+    plt.grid()
     file_name = f"{from_currency}_{to_currency}.png"
     plt.savefig(file_name)
     return file_name
