ed5edb71ba1186ff410f7a17ac91b883cebcd7e8
smurfikk <95306488+smurfikk@users.noreply.github.com>
2023-08-29 12:14:35 +0300

    fixed typos

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -6,7 +6,7 @@ import config
 def main():
     markup = InlineKeyboardMarkup(row_width=2)
     markup.add(
-        InlineKeyboardButton(text="Numbers/TON", callback_data="draw_graph:numbers:ton"),
+        InlineKeyboardButton(text="Number/TON", callback_data="draw_graph:number:ton"),
         InlineKeyboardButton(text="TON/USDT", callback_data="draw_graph:ton:usdt"),
         InlineKeyboardButton(text="USDT/RUB", callback_data="draw_graph:usdt:rub"),
         InlineKeyboardButton(text="TON/RUB", callback_data="draw_graph:ton:rub"),
@@ -21,7 +21,8 @@ def select_type_graph(from_currency: str, to_currency: str):
     markup = InlineKeyboardMarkup(row_width=2)
     markup.add(
         InlineKeyboardButton(text="За 2 дня", callback_data=f"draw_graph:{from_currency}:{to_currency}:short"),
-        InlineKeyboardButton(text="За месяц", cafllfback_data=f"draw_graph:{from_currency}:{to_currency}:long"),
+        InlineKeyboardButton(text="За месяц", callback_data=f"draw_graph:{from_currency}:{to_currency}:long"),
+        InlineKeyboardButton(text="Назад", callback_data=f"draw_graph"),
     )
     return markup
 
